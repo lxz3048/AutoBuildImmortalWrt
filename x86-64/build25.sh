85eefc2b80d26a777754d20997a9532339f7d0af
lxz3048 <48746152+lxz3048@users.noreply.github.com>
2026-06-14 20:19:05 +0800

    Update build25.sh

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -56,6 +56,12 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+#自添加
+PACKAGES="$PACKAGES luci-i18n-privoxy-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-minidlna-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-netdata-zh-cn"
 
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
